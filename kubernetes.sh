992b1bd357abf11063c79d8875c1389d9a7b558d
Praneeth Reddy <praneeth.kotholla@gmail.com>
2026-01-18 11:35:50 +0530

    installing helm

--- kubernetes.sh
+++ kubernetes.sh
@@ -42,3 +42,7 @@ ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 # install k9s
 curl -sS https://webinstall.dev/k9s | bash
 
+# installing Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
